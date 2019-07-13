8bb83e8231c545c5f4b8089a2c2e361e8adec5ab
egd1753 <egd1753@gmail.com>
2019-07-13 16:06:25 -0500

    fixed policy bug in index view

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -12,28 +12,37 @@ class WikiPolicy < ApplicationPolicy
      end
 
 
-     def resolve
-       wikis = []
-       if user.admin?
-         wikis = scope.all # if the user is an admin, show them all the wikis
-       elsif user.premium?
-         all_wikis = scope.all
-         all_wikis.each do |wiki|
-           if !wiki.private? || wiki.owner == user || wiki.collaborators.include?(user)
-             wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
-           end
-         end
-       else # this is the lowly standard user
-         all_wikis = scope.all
-         wikis = []
-         all_wikis.each do |wiki|
-           if !wiki.private? || wiki.collaborators.include?(user)
-             wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
-           end
-         end
-       end
-       wikis # return the wikis array we've built up
-     end
-   end
+    def resolve
+      if @user.admin? || @user.premium?
+        scope.all
+      else
+        scope.where(private: false)
+      end
+    end
+
+    def resolve
+      wikis = []
+      if @user.admin? 
+        wikis = scope.all # if the user is an admin, show them all the wikis
+      elsif @user.premium?
+        all_wikis = scope.all
+        all_wikis.each do |wiki|
+          if !wiki.private? || wiki.owner == @user || wiki.collaborators.include?(@user)
+            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
+          end
+        end
+      else # this is the lowly standard user
+        all_wikis = scope.all
+        wikis = []
+        all_wikis.each do |wiki|
+          if !wiki.private? || wiki.collaborators.include?(@user)
+            wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
+          end
+        end
+      end
+      wikis # return the wikis array we've built up
+    end
+
+  end
 
 end
