2596fdd6603dbfaa6ed805bca239ed9b833ca6a1
egd1753 <egd1753@gmail.com>
2019-07-15 11:35:12 -0500

    collaborators can now be added, working on validating uniqueness

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -6,11 +6,20 @@ class CollaboratorsController < ApplicationController
 
 # POST
   def create
-    @collaborator = Collaborator.new
-    @collaborator.wiki_id = params[:wiki_id]
-    collab_email = User.find_by(email: params[:add_collaborator])
-    @collaborator.user_id = collab_email
-    @collaborator.save!
+    @collab_user = User.find_by(email: params[:add_collaborator])
+    @wiki = Wiki.find(params[:wiki_id])
+    if @wiki.collaborators.exists?(user_id: params[:add_collaborator])
+      flash.now[:alert] = "This user is already a collaborator for this wiki."
+    else
+      @collaborator = Collaborator.new(user_id: @collab_user.id, wiki_id: @wiki.id)
+      @collaborator.save!
+      flash[:notice] = "You have successfully added #{params[:add_collaborator]} as a collaborator."
+    end
+#    if @collaborator.save!
+
+#    else
+#      flash.now[:alert] = "An error has occured. Make sure you entered the correct email and you have an internet connection."
+#    end
   end
 
 end
