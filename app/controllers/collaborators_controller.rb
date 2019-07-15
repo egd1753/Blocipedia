ad11e8bc854c85f4a00cc47c09732253854faa94
egd1753 <egd1753@gmail.com>
2019-07-15 12:21:01 -0500

    updated

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -8,12 +8,14 @@ class CollaboratorsController < ApplicationController
   def create
     @collab_user = User.find_by(email: params[:add_collaborator])
     @wiki = Wiki.find(params[:wiki_id])
-    if @wiki.collaborators.exists?(user_id: params[:add_collaborator])
+    if @wiki.collaborators.exists?(user_id: @collab_user.id)
       flash.now[:alert] = "This user is already a collaborator for this wiki."
+      redirect_to @wiki
     else
       @collaborator = Collaborator.new(user_id: @collab_user.id, wiki_id: @wiki.id)
       @collaborator.save!
       flash[:notice] = "You have successfully added #{params[:add_collaborator]} as a collaborator."
+      redirect_to @wiki
     end
 #    if @collaborator.save!
 
