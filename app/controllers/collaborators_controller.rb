df00853f40a119d6732aef41b6d576c411f60ef3
egd1753 <egd1753@gmail.com>
2019-07-18 11:04:27 -0500

    working on delete method for collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,5 +1,6 @@
 class CollaboratorsController < ApplicationController
 
+
   def new
     @collaborator = Collaborator.new
   end
@@ -9,7 +10,7 @@ class CollaboratorsController < ApplicationController
     @collab_user = User.find_by(email: params[:add_collaborator])
     @wiki = Wiki.find(params[:wiki_id])
     if @wiki.collaborators.exists?(user_id: @collab_user.id)
-      flash.now[:alert] = "This user is already a collaborator for this wiki."
+      flash[:alert] = "This user is already a collaborator for this wiki."
       redirect_to @wiki
     else
       @collaborator = Collaborator.new(user_id: @collab_user.id, wiki_id: @wiki.id)
@@ -17,11 +18,21 @@ class CollaboratorsController < ApplicationController
       flash[:notice] = "You have successfully added #{params[:add_collaborator]} as a collaborator."
       redirect_to @wiki
     end
-#    if @collaborator.save!
+  end
 
-#    else
-#      flash.now[:alert] = "An error has occured. Make sure you entered the correct email and you have an internet connection."
-#    end
+
+  def destroy
+    @collab_user = User.find_by(email: params[:remove_collaborator])
+    @wiki = Wiki.find(params[:wiki_id])
+    @collaborator = @wiki.collaborators.where(user_id: @collab_user.id)
+    if @collaborator
+      @collaborator.destroy
+      flash[:notice] = "You have successfully removed #{params[:remove_collaborator]} from this wiki."
+      redirect_to @wiki
+    else
+      flash[:notice] = "Error, either: #{params[:remove_collaborator]} was spelled wrong, or #{params[:remove_collaborator]} is not a collaborator on this wiki."
+      redirect_to @wiki
+    end
   end
 
 end
